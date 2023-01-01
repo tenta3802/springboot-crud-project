c36e205b1ecf61630a61aef980288f9d06ce9468
tenta <eunoia3802@gmail.com>
2023-01-01 23:21:49 +0900

    mapper 인터페이스 생성

--- src/main/java/com/java/crud/domain/post/PostMapper.java
+++ src/main/java/com/java/crud/domain/post/PostMapper.java
@@ -1,5 +1,10 @@
 package com.java.crud.domain.post;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface PostMapper {
     /**
      * 게시글 저장
@@ -7,4 +12,34 @@ public interface PostMapper {
      */
     void save(PostRequest params);
 
+    /**
+     * 게시글 상세정보 조회
+     * @param id - PK
+     * @return 게시글 상세정보
+     */
+    PostResponse findById(Long id);
+
+    /**
+     * 게시글 수정
+     * @param params - 게시글 정보
+     */
+    void update(PostRequest params);
+
+    /**
+     * 게시글 삭제
+     * @param id - Pk
+     */
+    void deleteById(Long id);
+
+    /**
+     * 게시글 리스트 조회
+     * @return 게시글 리스트
+     */
+    List<PostResponse> findAll();
+
+    /**
+     * 게시글 수 카운팅
+     * @return 게시글 수
+     */
+    int count();
 }
