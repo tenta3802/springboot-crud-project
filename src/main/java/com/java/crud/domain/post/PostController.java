e9e167a55362f96ab24f225650cc11968ac31c38
tenta <eunoia3802@gmail.com>
2023-01-04 11:21:05 +0900

    게시글 생성을 위한 입력 form 작성

--- src/main/java/com/java/crud/domain/post/PostController.java
+++ src/main/java/com/java/crud/domain/post/PostController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,14 +13,12 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping(value = "/post/write.do")
-    public String openPostWrite(Model model) {
-//        String title = "제목";
-//        String content = "내용";
-//        String writer = "홍길동";
-//
-//        model.addAttribute("title", title);
-//        model.addAttribute("content", content);
-//        model.addAttribute("writer", writer);
+    public String openPostWrite(@RequestParam(value = "id", required = false)
+                                final Long id, Model model) {
+        if(id!=null) {
+            PostResponse post = postService.findPostById(id);
+            model.addAttribute("post", post);
+        }
         return "post/write";
     }
 }
