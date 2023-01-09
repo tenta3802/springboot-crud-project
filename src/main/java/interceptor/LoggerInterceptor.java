e0c6fbaafd2409f0af47f2b41e1b5228f82c66e5
tenta <eunoia3802@gmail.com>
2023-01-09 23:57:50 +0900

    인터셉터 적용: 정적 리소스 파일 제외

---
+++ src/main/java/interceptor/LoggerInterceptor.java
@@ -0,0 +1,28 @@
+package interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class LoggerInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.debug("===============================================");
+        log.debug("==================== BEGIN ====================");
+        log.debug("Request URI ===> " + request.getRequestURI());
+
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        log.debug("==================== END ======================");
+        log.debug("===============================================");
+    }
+}
